508fc830e42446fc504b7ee879bb868944007baa
Shane Honanie <shaneh425@gmail.com>
2019-02-19 11:56:56 -0800

    Small Fixes to OrderSummary, Modal & Backdrop

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
 import Auxillary from '../../../hoc/Auxillary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
 	const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
 		return (
 			<li key={igKey}>
-				<span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
+				<span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
 				{props.ingredients[igKey]}
 			</li>
 		);
 	});
+
 	return (
 		<Auxillary>
 			<h3>Your Order</h3>
-			<p>A delicous burger with the following ingredients:</p>
-			<ul>{ingredientSummary} </ul>
+			<p>A delicious burger with the following ingredients:</p>
+			<ul>{ingredientSummary}</ul>
 			<p>Continue to Checkout?</p>
-			<button>CANCEL</button>
-			<button>CONTINUE</button>
+			<Button btnType='Danger' clicked={props.purchaseCancelled}>
+				CANCEL
+			</Button>
+			<Button btnType='Success' clicked={props.purchaseContinued}>
+				CONTINUE
+			</Button>
 		</Auxillary>
 	);
 };
