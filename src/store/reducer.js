9589627e0896269cdbdb5d04ac522aedb1a8c4e7
Shane Honanie <shaneh425@gmail.com>
2019-02-25 17:39:35 -0800

    Redux Setup

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+	ingredients: null,
+	totalPrice: null
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
