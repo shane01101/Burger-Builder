526aa54d53a83e0b6d00f2421255e392484b5c07
Shane Honanie <shaneh425@gmail.com>
2019-02-27 11:46:48 -0800

    Init Ingredredients from Firebase & Error Handling

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -31,7 +31,7 @@ export const fetchIngredientsFailed = () => {
 export const initIngredients = () => {
 	return dispatch => {
 		axios
-			.get('https://react-my-burger.firebaseio.com/ingredients.json')
+			.get('https://react-my-burger-29d87.firebaseio.com/ingredients.json')
 			.then(response => {
 				dispatch(setIngredients(response.data));
 			})
