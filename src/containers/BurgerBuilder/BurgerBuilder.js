7dbceef6a0625e5122821589cdd25e699b70b85e
Shane Honanie <shaneh425@gmail.com>
2019-02-26 18:22:14 -0800

    Fetch Ingredients from Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,8 +8,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import axios from '../../axios-orders';
 import * as burgerBuilderActions from '../../store/actions/index';
+import axios from '../../axios-orders';
 
 class BurgerBuilder extends Component {
 	// constructor(props) {
@@ -17,20 +17,11 @@ class BurgerBuilder extends Component {
 	//     this.state = {...}
 	// }
 	state = {
-		purchasing: false,
-		loading: false,
-		error: false
+		purchasing: false
 	};
 
 	componentDidMount() {
 		console.log(this.props);
-		// axios.get( 'https://react-my-burger.firebaseio.com/ingredients.json' )
-		//     .then( response => {
-		//         this.setState( { ingredients: response.data } );
-		//     } )
-		//     .catch( error => {
-		//         this.setState( { error: true } );
-		//     } );
 	}
 
 	updatePurchaseState(ingredients) {
@@ -92,9 +83,6 @@ class BurgerBuilder extends Component {
 					purchaseContinued={this.purchaseContinueHandler}
 				/>
 			);
-		}
-		if (this.state.loading) {
-			orderSummary = <Spinner />;
 		}
 		// {salad: true, meat: false, ...}
 		return (
