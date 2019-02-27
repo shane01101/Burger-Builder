7c951fcdefb9feab3e101cbd82ebff04d5c62322
Shane Honanie <shaneh425@gmail.com>
2019-02-27 14:47:25 -0800

    Purchase Order Redirect

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -18,8 +18,12 @@ export class Checkout extends Component {
 		let summary = <Redirect to='/' />;
 
 		if (this.props.ings) {
+			const purchasedRedirect = this.props.purchased ? (
+				<Redirect to='/' />
+			) : null;
 			summary = (
 				<div>
+					{purchasedRedirect}
 					<CheckoutSummary
 						ingredients={this.props.ings}
 						checkoutCancelled={this.checkoutCancelledHandler}
@@ -39,7 +43,8 @@ export class Checkout extends Component {
 
 const mapStateToProps = state => {
 	return {
-		ings: state.burgerBuilder.ingredients
+		ings: state.burgerBuilder.ingredients,
+		purchased: state.order.purchased
 	};
 };
 
