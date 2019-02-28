0ccc041ce76d5250506ac19fb59765a30ddca7ea
Shane Honanie <shaneh425@gmail.com>
2019-02-28 12:02:44 -0800

    Auth Sign-up User

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,23 @@ export const authFail = error => {
 export const auth = (email, password) => {
 	return dispatch => {
 		dispatch(authStart());
+		const authData = {
+			email: email,
+			password: password,
+			returnSecureToken: true
+		};
+		axios
+			.post(
+				'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCRFIrVTuldZQ1Fu9q9Lk7R3RZ1Na0NDjs',
+				authData
+			)
+			.then(response => {
+				console.log(response);
+				dispatch(authSuccess(response.data));
+			})
+			.catch(err => {
+				console.log(err);
+				dispatch(authFail(err));
+			});
 	};
 };
