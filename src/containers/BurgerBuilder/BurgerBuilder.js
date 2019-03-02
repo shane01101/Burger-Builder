d99e919d286f875b72e1454af313d58695fcb47d
Shane Honanie <shaneh425@gmail.com>
2019-03-02 13:00:12 -0800

    Redirect Unauthenticated Users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,7 +37,8 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseHandler = () => {
-		this.setState({ purchasing: true });
+		if (this.props.isAuthenticated) this.setState({ purchasing: true });
+		else this.props.history.push('/auth');
 	};
 
 	purchaseCancelHandler = () => {
@@ -73,6 +74,7 @@ class BurgerBuilder extends Component {
 						disabled={disabledInfo}
 						purchasable={this.updatePurchaseState(this.props.ings)}
 						ordered={this.purchaseHandler}
+						isAuth={this.props.isAuthenticated}
 						price={this.props.price}
 					/>
 				</Auxillary>
@@ -105,7 +107,8 @@ const mapStateToProps = state => {
 	return {
 		ings: state.burgerBuilder.ingredients,
 		price: state.burgerBuilder.totalPrice,
-		error: state.burgerBuilder.error
+		error: state.burgerBuilder.error,
+		isAuthenticated: state.auth.token !== null
 	};
 };
 
